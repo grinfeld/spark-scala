57991689437af7788dffc16e096dd6a0d24f0f67
Mikhail <grinfeld@gmail.com>
2021-12-06 16:49:12 +0200

    working version

--- src/main/scala/com/mikerusoft/spark/scala/test/MyCode.scala
+++ src/main/scala/com/mikerusoft/spark/scala/test/MyCode.scala
@@ -1,11 +1,12 @@
 package com.mikerusoft.spark.scala.test
 
-import io.lettuce.core.RedisClient
 import org.apache.spark.sql.SparkSession
 import org.slf4j.LoggerFactory
 
 class MyCode() extends Serializable {
   @transient lazy val log = LoggerFactory.getLogger("MyCode")
+  @transient lazy val redis = new RedisFactory()
+
   def run():Unit = {
     val sparkSession = SparkSession.builder.appName("test").master("local[*]")
       .config("spark.executor.cores", "4")
@@ -14,18 +15,16 @@ class MyCode() extends Serializable {
     import sparkSession.implicits._
     val dataset = sparkSession.createDataset(List(1,2,3))
 
-    val redisClient = RedisClient.create("redis://localhost:6379/")
-    val redis = redisClient.connect.sync()
 
     dataset.
       foreachPartition((it: Iterator[Int]) => it.foreach(i => {
-        redis.set(i.toString, s"${(i * 5).toString}")
-        val res: String = redis.get(i.toString)
+        redis.redisCommands().set(i.toString, s"${(i * 5).toString}")
+        val res: String = redis.redisCommands().get(i.toString)
         log.info(s"<<<<<<Success -> ${res}")
       }))
 
-    redis.set("driver", "hello")
-    val res: String = redis.get("driver")
+    redis.redisCommands().set("driver", "hello")
+    val res: String = redis.redisCommands().get("driver")
     log.info(s">>>>>>>Success Driver -> ${res}")
   }
 }
