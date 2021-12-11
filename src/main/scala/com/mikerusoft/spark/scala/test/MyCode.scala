bda095f05d6787e860f85bc28ddc44e361eb4890
Mikhail <grinfeld@gmail.com>
2021-12-11 08:20:37 +0200

    lifecycle submit

--- src/main/scala/com/mikerusoft/spark/scala/test/MyCode.scala
+++ src/main/scala/com/mikerusoft/spark/scala/test/MyCode.scala
@@ -1,12 +1,14 @@
 package com.mikerusoft.spark.scala.test
 
 import io.lettuce.core.RedisClient
+import io.lettuce.core.api.sync.RedisCommands
 import org.apache.spark.sql.SparkSession
 import org.slf4j.{Logger, LoggerFactory}
 
 class MyCode() extends Serializable {
   @transient lazy val log: Logger = LoggerFactory.getLogger("MyCode")
   //@transient lazy val redis = new RedisFactory()
+  @transient lazy val redis: RedisCommands[String, String] = RedisClient.create("redis://localhost:6379/").connect().sync()
 
   def run():Unit = {
     val sparkSession = SparkSession.builder.appName("test").master("local[*]")
@@ -18,7 +20,7 @@ class MyCode() extends Serializable {
 
     dataset
       .foreachPartition((it: Iterator[Pojo]) => it.foreach(p => {
-        val redis = RedisClient.create("redis://localhost:6379/").connect().sync()
+        //val redis = RedisClient.create("redis://localhost:6379/").connect().sync()
         redis.set(p.i.toString, s"${(p.i * 5).toString}")
         val res: String = redis.get(p.i.toString)
         log.info(s"<<<<<<Success -> ${res}")
